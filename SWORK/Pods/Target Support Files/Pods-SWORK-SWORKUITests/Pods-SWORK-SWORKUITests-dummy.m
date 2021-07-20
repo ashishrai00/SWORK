36741bef02016a8041ccb7cec1e4d2638fae17a4
Aasheesh Rai <ashishrai00>
2021-07-20 20:44:42 +0530

    Removed pod file

--- SWORK/Pods/Target Support Files/Pods-SWORK-SWORKUITests/Pods-SWORK-SWORKUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SWORK_SWORKUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SWORK_SWORKUITests
-@end
--- SWORK/Pods/Target Support Files/Pods-SWORKTests/Pods-SWORKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SWORKTests : NSObject
-@end
-@implementation PodsDummy_Pods_SWORKTests
-@end
